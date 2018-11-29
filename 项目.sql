87425a3a4e493e91f6e5f55e7fd932df04169a38
Akanemiku <33932911+Akanemiku@users.noreply.github.com>
2018-11-29 17:25:41 +0800

    Update 项目.sql

--- 项目.sql
+++ 项目.sql
@@ -37,3 +37,10 @@ create table Stuff
 	sphone varchar(20),
 	position varchar(20) not null
 )
+
+insert into Manufacturer values(1000,'江南皮革厂','浙江温州','12345678')
+insert into Manufacturer values(1001,'哇哈哈集团有限公司','浙江杭州','87654321')
+insert into Goods values(1,1000,'牛皮大衣',9999)
+insert into Goods values(2,1001,'哇哈哈矿泉水',1)
+insert into Stock values(1,2)
+insert into Stock values(2,100)
